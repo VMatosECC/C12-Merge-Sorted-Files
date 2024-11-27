814c7d62ae0d146d8e1f0a59e5cd08c8414accc8
Victor Matos <vmatos@elcamino.edu>
2024-11-27 09:07:08 -0800

    Variables renamed, file type corrected - all of then are text files

--- C12-Merge-Sorted-Files.cpp
+++ C12-Merge-Sorted-Files.cpp
@@ -1,5 +1,10 @@
 // C12-Merge-Sorted-Files.cpp 
 /*
+This app merges two sorted files into a larger, sorted file.
+It compares the leading elements of the two files and writes the smaller
+to the ouput file. It repeats this process with the rest of the records.
+
+TECHNICAL NOTE
 file.eof() does not become true immediately upon reaching the end of the file, 
 but rather after a read operation tries to go past the end.
 */
@@ -17,57 +22,57 @@ int main()
     string fileName2 = "c:/temp/mergePart2.txt";   //input file
     string fileName3 = "c:/temp/mergePart3.txt";   //output file
 
-    fstream file1 = openFile(fileName1, ios::in | ios::binary);
-    fstream file2 = openFile(fileName2, ios::in | ios::binary);
+    fstream file1 = openFile(fileName1, ios::in );
+    fstream file2 = openFile(fileName2, ios::in );
     fstream file3 = openFile(fileName3, ios::out);
 
     int num1, num2;
-    //read next value from file1 and file2
-    bool hasNum1 = (bool)(file1 >> num1);
-    bool hasNum2 = (bool)(file2 >> num2);
+    //read next values from file1 and file2, sets availability flags
+    bool num1Available = (bool)(file1 >> num1);
+    bool num2Available = (bool)(file2 >> num2);
 
-    while (hasNum1 && hasNum2)
+    while (num1Available && num2Available)
     {
         if (num1 < num2) {
             file3 << num1 << endl;
-            hasNum1 = (bool)(file1 >> num1);
+            num1Available = (bool)(file1 >> num1);
         }
         else {
             file3 << num2 << endl;
-            hasNum2 = (bool)(file2 >> num2);
+            num2Available = (bool)(file2 >> num2);
         }
     }
 
-    while (hasNum1)
+    while (num1Available)
     {
         file3 << num1 << endl;
-        hasNum1 = (bool)(file1 >> num1);
+        num1Available = (bool)(file1 >> num1);
     }
 
-    while (hasNum2)
+    while (num2Available)
     {
         file3 << num2 << endl;
-        hasNum2 = (bool)(file2 >> num2);
+        num2Available = (bool)(file2 >> num2);
     }
 
-
-
+    //Done - close files
     file1.close();
     file2.close();
     file3.close();
 
-    //show the merged file
+    //DEBUG - show the merged file
     file3 = openFile(fileName3);
     int num;
-    while (file3 >> num)
-    {
+    while (file3 >> num) {
         cout << num << endl;
     }
     cout << "\nAll done!\n";
 }
 
+//--------------------------------------------------------------------------
 fstream openFile(string fileName, ios::openmode mode)
 {
+    //Utility function to open a file with a given mode
     fstream file (fileName, mode);
     if (!file)
     {
